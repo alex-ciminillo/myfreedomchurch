f9b8400772a6757b49b9371d0224773ab9c8b4bf
TheSicilian12 <mguidera314@gmail.com>
2024-01-25 09:02:40 -0500

    general style for beliefs details

---
+++ dist/index.84927896b35912b83b9a.hot-update.js
@@ -0,0 +1,27 @@
+"use strict";
+self["webpackHotUpdatefreedom_church"]("index",{
+
+/***/ "./src/pages/beliefs/beliefspage.html":
+/*!********************************************!*\
+  !*** ./src/pages/beliefs/beliefspage.html ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "5a04e5b5bbf90f4a3f4733177f535cf5.html");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("50d732a7fab065e7d9c9")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguODQ5Mjc4OTZiMzU5MTJiODNiOWEuaG90LXVwZGF0ZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7O0FBQUEsaUVBQWUscUJBQXVCLDBDQUEwQzs7Ozs7Ozs7VUNBaEYiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9mcmVlZG9tX2NodXJjaC8uL3NyYy9wYWdlcy9iZWxpZWZzL2JlbGllZnNwYWdlLmh0bWwiLCJ3ZWJwYWNrOi8vZnJlZWRvbV9jaHVyY2gvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IF9fd2VicGFja19wdWJsaWNfcGF0aF9fICsgXCI1YTA0ZTViNWJiZjkwZjRhM2Y0NzMzMTc3ZjUzNWNmNS5odG1sXCI7IiwiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiNTBkNzMyYTdmYWIwNjVlN2Q5YzlcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
