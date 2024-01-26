b955b1914ecf5ea020d9783d8cb01ad950646366
TheSicilian12 <mguidera314@gmail.com>
2024-01-26 08:48:16 -0500

    about and the gospel sections

---
+++ dist/index.d0d7b1d9321f5a317b91.hot-update.js
@@ -0,0 +1,27 @@
+"use strict";
+self["webpackHotUpdatefreedom_church"]("index",{
+
+/***/ "./src/pages/beliefs/beliefspage.html":
+/*!********************************************!*\
+  !*** ./src/pages/beliefs/beliefspage.html ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "750471c3f4c263db238cebd5e226cdf8.html");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("cc8eb2cd590617526b88")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguZDBkN2IxZDkzMjFmNWEzMTdiOTEuaG90LXVwZGF0ZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7O0FBQUEsaUVBQWUscUJBQXVCLDBDQUEwQzs7Ozs7Ozs7VUNBaEYiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9mcmVlZG9tX2NodXJjaC8uL3NyYy9wYWdlcy9iZWxpZWZzL2JlbGllZnNwYWdlLmh0bWwiLCJ3ZWJwYWNrOi8vZnJlZWRvbV9jaHVyY2gvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IF9fd2VicGFja19wdWJsaWNfcGF0aF9fICsgXCI3NTA0NzFjM2Y0YzI2M2RiMjM4Y2ViZDVlMjI2Y2RmOC5odG1sXCI7IiwiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiY2M4ZWIyY2Q1OTA2MTc1MjZiODhcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
