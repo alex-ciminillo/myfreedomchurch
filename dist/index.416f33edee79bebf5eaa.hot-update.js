21db465bae2b637f9aba79780e13c23f4e9bc97a
TheSicilian12 <mguidera314@gmail.com>
2024-01-26 08:56:30 -0500

    style for about, the gospels, beliefs, there is a bug with the underlining. You can just select the underlining div and get the system off

---
+++ dist/index.416f33edee79bebf5eaa.hot-update.js
@@ -0,0 +1,27 @@
+"use strict";
+self["webpackHotUpdatefreedom_church"]("index",{
+
+/***/ "./src/pages/beliefs/beliefspage.html":
+/*!********************************************!*\
+  !*** ./src/pages/beliefs/beliefspage.html ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "d439b4a0fb080f77814eb6c64de13aa3.html");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("dc2f2f4f4a9dd1425586")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguNDE2ZjMzZWRlZTc5YmViZjVlYWEuaG90LXVwZGF0ZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7O0FBQUEsaUVBQWUscUJBQXVCLDBDQUEwQzs7Ozs7Ozs7VUNBaEYiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9mcmVlZG9tX2NodXJjaC8uL3NyYy9wYWdlcy9iZWxpZWZzL2JlbGllZnNwYWdlLmh0bWwiLCJ3ZWJwYWNrOi8vZnJlZWRvbV9jaHVyY2gvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IF9fd2VicGFja19wdWJsaWNfcGF0aF9fICsgXCJkNDM5YjRhMGZiMDgwZjc3ODE0ZWI2YzY0ZGUxM2FhMy5odG1sXCI7IiwiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiZGMyZjJmNGY0YTlkZDE0MjU1ODZcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ dist/index.937da19b6e252b7f7310.hot-update.js
@@ -0,0 +1,27 @@
+"use strict";
+self["webpackHotUpdatefreedom_church"]("index",{
+
+/***/ "./src/pages/beliefs/beliefspage.html":
+/*!********************************************!*\
+  !*** ./src/pages/beliefs/beliefspage.html ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "7d952d383871b7bdf66221f4dbc8a5f3.html");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("dd3bf5df6bbc0396f401")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguOTM3ZGExOWI2ZTI1MmI3ZjczMTAuaG90LXVwZGF0ZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7O0FBQUEsaUVBQWUscUJBQXVCLDBDQUEwQzs7Ozs7Ozs7VUNBaEYiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9mcmVlZG9tX2NodXJjaC8uL3NyYy9wYWdlcy9iZWxpZWZzL2JlbGllZnNwYWdlLmh0bWwiLCJ3ZWJwYWNrOi8vZnJlZWRvbV9jaHVyY2gvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IF9fd2VicGFja19wdWJsaWNfcGF0aF9fICsgXCI3ZDk1MmQzODM4NzFiN2JkZjY2MjIxZjRkYmM4YTVmMy5odG1sXCI7IiwiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiZGQzYmY1ZGY2YmJjMDM5NmY0MDFcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
