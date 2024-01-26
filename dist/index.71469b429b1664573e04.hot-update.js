8e1cf3f8e658923155ef3835ecc0ef69852aebcf
TheSicilian12 <mguidera314@gmail.com>
2024-01-26 08:38:52 -0500

    adjusted some of the gospel divs, were not displaying correctly

---
+++ dist/index.71469b429b1664573e04.hot-update.js
@@ -0,0 +1,27 @@
+"use strict";
+self["webpackHotUpdatefreedom_church"]("index",{
+
+/***/ "./src/pages/beliefs/beliefspage.html":
+/*!********************************************!*\
+  !*** ./src/pages/beliefs/beliefspage.html ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "4fed3cf87bca71f25dfe155df09b5687.html");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("8e59f50826c28f4fe1e1")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguNzE0NjliNDI5YjE2NjQ1NzNlMDQuaG90LXVwZGF0ZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7O0FBQUEsaUVBQWUscUJBQXVCLDBDQUEwQzs7Ozs7Ozs7VUNBaEYiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9mcmVlZG9tX2NodXJjaC8uL3NyYy9wYWdlcy9iZWxpZWZzL2JlbGllZnNwYWdlLmh0bWwiLCJ3ZWJwYWNrOi8vZnJlZWRvbV9jaHVyY2gvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IF9fd2VicGFja19wdWJsaWNfcGF0aF9fICsgXCI0ZmVkM2NmODdiY2E3MWYyNWRmZTE1NWRmMDliNTY4Ny5odG1sXCI7IiwiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiOGU1OWY1MDgyNmMyOGY0ZmUxZTFcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
