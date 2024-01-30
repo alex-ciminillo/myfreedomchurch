1d1803eda64b1de752e985ad0c6e69795c0dd4fd
TheSicilian12 <mguidera314@gmail.com>
2024-01-30 12:25:55 -0500

    section-2-pt-3-details arrow was not working fixed

---
+++ dist/index.8be6266103102be61412.hot-update.js
@@ -0,0 +1,27 @@
+"use strict";
+self["webpackHotUpdatefreedom_church"]("index",{
+
+/***/ "./src/pages/beliefs/beliefspage.html":
+/*!********************************************!*\
+  !*** ./src/pages/beliefs/beliefspage.html ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "39be82647c84e1c021b3991e3003818e.html");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("c80c6e87c4328164962d")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguOGJlNjI2NjEwMzEwMmJlNjE0MTIuaG90LXVwZGF0ZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7O0FBQUEsaUVBQWUscUJBQXVCLDBDQUEwQzs7Ozs7Ozs7VUNBaEYiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9mcmVlZG9tX2NodXJjaC8uL3NyYy9wYWdlcy9iZWxpZWZzL2JlbGllZnNwYWdlLmh0bWwiLCJ3ZWJwYWNrOi8vZnJlZWRvbV9jaHVyY2gvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IF9fd2VicGFja19wdWJsaWNfcGF0aF9fICsgXCIzOWJlODI2NDdjODRlMWMwMjFiMzk5MWUzMDAzODE4ZS5odG1sXCI7IiwiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiYzgwYzZlODdjNDMyODE2NDk2MmRcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
